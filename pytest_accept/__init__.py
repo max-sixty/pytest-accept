4ea599e9e12d72947e9ce7833d04c6d2457710ee
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2021-09-06 15:36:55 -0700

    Don't overwrite modified files (#17)
    * Don't overwrite modified files

--- pytest_accept/__init__.py
+++ pytest_accept/__init__.py
@@ -1,13 +1,18 @@
 from .doctest_plugin import (
     pytest_addoption,
+    pytest_collect_file,
     pytest_configure,
     pytest_runtest_makereport,
     pytest_sessionfinish,
 )
+
+# Note that pluginsn need to be listed here in order for pytest to pick them up when
+# this package is installed.
 
 __all__ = [
     pytest_runtest_makereport,
     pytest_sessionfinish,
     pytest_addoption,
     pytest_configure,
+    pytest_collect_file,
 ]
