665b4a8c6393d7e1183b4ec90f7742de5920611e
Maximilian Roos <m@maxroos.com>
2021-12-04 17:14:44 -0800

    Add docs on raw strings & escape characters

---
+++ examples/escapes.py
@@ -0,0 +1,45 @@
+def func(a, b):
+    r"""
+    Example:
+    >>> import xarray as xr
+    >>> x = func(xr.DataArray([5], dims='a'), xr.DataArray([1], dims='a'))
+    >>> s = x.__repr__()
+    >>> x
+    <xarray.DataArray (a: 1)>
+    array([6])
+    Dimensions without coordinates: a
+    Attributes:
+        a:        <xarray.DataArray (a: 1)>\narray([5])\nDimensions without coord...
+        b:        <xarray.DataArray (a: 1)>\narray([1])\nDimensions without coord...
+    >>> len(s)
+    244
+    >>> print(s)
+    <xarray.DataArray (a: 1)>
+    array([6])
+    Dimensions without coordinates: a
+    Attributes:
+        a:        <xarray.DataArray (a: 1)>\narray([5])\nDimensions without coord...
+        b:        <xarray.DataArray (a: 1)>\narray([1])\nDimensions without coord...
+
+    >>> print(x.__repr__())
+    <xarray.DataArray (a: 1)>
+    array([6])
+    Dimensions without coordinates: a
+    Attributes:
+        a:        <xarray.DataArray (a: 1)>\narray([5])\nDimensions without coord...
+        b:        <xarray.DataArray (a: 1)>\narray([1])\nDimensions without coord...
+
+    >>> r"testing\ntesting"
+    'testing\\ntesting'
+    """
+    c = a + b
+    c = c.assign_attrs(a=a, b=b)
+    return c
+
+
+def raw_string():
+    r"""
+    >>> "\n"
+    '\n'
+
+    """
