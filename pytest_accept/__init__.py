c1cf94cf4f8c242660458e34e1fddbb0056602a4
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-07-30 14:32:10 +0200

    remove doctest

--- pytest_accept/__init__.py
+++ pytest_accept/__init__.py
@@ -1,10 +1,5 @@
 import pytest
 
-from .doctest_plugin import (
-    pytest_collect_file,
-    pytest_configure,
-)
-
 from .assert_plugin import pytest_assertrepr_compare
 
 # Note that plugins need to be listed here in order for pytest to pick them up when
@@ -15,18 +10,12 @@ from .assert_plugin import pytest_assertrepr_compare
 def pytest_runtest_makereport(item, call):
     outcome = yield
 
-    from .doctest_plugin import pytest_runtest_makereport
-    pytest_runtest_makereport(item, call, outcome)
-
     from .assert_plugin import pytest_runtest_makereport
     pytest_runtest_makereport(item, call, outcome)
     # returning this is required by pytest
     return outcome.get_result()
 
 def pytest_sessionfinish(session, exitstatus):
-    from .doctest_plugin import pytest_sessionfinish
-    pytest_sessionfinish(session, exitstatus)
-
     from .assert_plugin import pytest_sessionfinish
     pytest_sessionfinish(session, exitstatus)
 
@@ -37,18 +26,16 @@ def pytest_addoption(parser):
         "--accept",
         action="store_true",
         default=False,
-        help="Accept the output of doctests, overwriting python files with generated results.",
+        help="Accept the output of asserts, overwriting python files with generated results.",
     )
     group.addoption(
         "--accept-copy",
         action="store_true",
         default=False,
-        help="Write a copy of python file named `.py.new` with the generated results of doctests.",
+        help="Write a copy of python file named `.py.new` with the generated results of asserts.",
     )
 
 __all__ = [
-    pytest_collect_file,
-    pytest_configure,
     pytest_assertrepr_compare,
     pytest_runtest_makereport,
     pytest_sessionfinish,
